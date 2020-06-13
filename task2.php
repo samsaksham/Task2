00a12ba2cca1c4ab71d8a94c6bfeff7ac4a54833
samsaksham <53097446+samsaksham@users.noreply.github.com>
2020-06-13 14:01:08 +0000

    Add files via upload

---
+++ task2.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+<title> it works</title>
+</head>
+
+<body bgcolor="orange" text="blue">
+
+<h1> The site works </h1>
+
+This is my first web page and I can say anything I want in here - I do that by putting text or images in the body section - where I'm typing right now :)
+
+</body>
+
+</html>
+Notes: 
